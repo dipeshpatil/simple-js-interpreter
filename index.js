2d72ee8a681db68b5a4e8988d0fd7a51d3194a4f
Dipesh Patil <heydipesh28@gmail.com>
2022-01-03 18:27:12 +0530

    Deleted Old Files

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-const util = require("util");
+import Lexer from "./Lexer/Lexer.js";
 
-const Lexer = require("./Lexer/lexer");
-const Parser = require("./Parser/parser");
-const Evaluate = require("./Evaluator/evaluate");
-const Order = require("./Evaluator/order");
+const text = "1 + 1";
+const tokens = new Lexer(text).generateTokens();
 
-const text = "";
-// const text = "O(b100 + b100)";
-
-const lexer = new Lexer(text);
-const tokens = lexer.generateTokens();
-
-const parser = new Parser(tokens);
-const tree = parser.parse();
-
-console.log("Expression\n" + text + "\n");
-console.log("Tokens");
 console.log(tokens);
-console.log(
-  "\nTree\n" + util.inspect(tree, { showHidden: false, depth: null })
-);
-console.log("\nOrder Of Evaluation => " + Order(tree));
-console.log("\nEvaluated Result\n" + Evaluate(tree));
--- new_/index.js
+++
@@ -1,6 +0,0 @@
-import Lexer from "./Lexer/Lexer.js";
-
-const text = "1 + a";
-const tokens = new Lexer(text).generateTokens();
-
-console.log(tokens);
